83ef4f2b5bbe6773949b349643f930a50b337084
Josh Dadak <joshdadak@gmail.com>
2021-04-17 14:00:49 +0100

    Added FluentParser for parsing command line aruguments
    I like this package as it lets you easily set up short and long name params and display a help menu for the user.

---
+++ WordLadder/ApplicationArguments.cs
@@ -0,0 +1,10 @@
+﻿namespace WordLadder
+{
+    public class ApplicationArguments
+    {
+        public string DictionaryFileName { get; set; }
+        public string ResultsFileName { get; set; }
+        public string StartWord { get; set; }
+        public string EndWord { get; set; }
+    }
+}
--- WordLadder/Program.cs
+++ WordLadder/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Fclp;
+using System;
+using System.Text.Json;
+using WordLadder;
 
 namespace Wordladder
 {
@@ -6,7 +9,47 @@ namespace Wordladder
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            var parser = SetupFluentCommandLineParser();
+            var parsedResults = parser.Parse(args);
+
+            if (parsedResults.HasErrors is false)
+            {
+                // Run the app
+                Console.WriteLine(JsonSerializer.Serialize(parser.Object));
+            }
+            else
+            {
+                Console.WriteLine(parsedResults.ErrorText);
+            }
+        }
+
+        private static FluentCommandLineParser<ApplicationArguments> SetupFluentCommandLineParser()
+        {
+            var parser = new FluentCommandLineParser<ApplicationArguments>();
+            parser.SetupHelp("?", "help")
+                  .Callback(text => Console.WriteLine(text));
+
+            parser.Setup(arg => arg.DictionaryFileName)
+             .As('d', "dictionary")
+             .WithDescription("The filename of the Dictionary File to use. Currently we only support .txt files.")
+             .Required();
+
+            parser.Setup(arg => arg.ResultsFileName)
+             .As('r', "result")
+             .WithDescription("The filename of the results .txt file.")
+             .Required();
+
+            parser.Setup(arg => arg.StartWord)
+             .As('s', "start")
+             .WithDescription("The Word to start the word ladder with.")
+             .Required();
+
+            parser.Setup(arg => arg.EndWord)
+             .As('e', "end")
+             .WithDescription("The Word to end the word ladder at.")
+             .Required();
+
+            return parser;
         }
     }
 }
